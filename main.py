81f07b533858b9f7314043cfd65693500d120db0
BeFunny1 <BeFunny1@users.noreply.github.com>
2020-10-10 01:03:14 +0500

    Add keyboard window

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import sys
+from PyQt5 import QtWidgets
+
+from visualization import WindowKeyboard
+
 if __name__ == '__main__':
-    pass
+    app = QtWidgets.QApplication(sys.argv)
+    KeyboardSimulator = QtWidgets.QDialog()
+    ui = WindowKeyboard()
+    ui.setupUi(KeyboardSimulator)
+    KeyboardSimulator.show()
+    sys.exit(app.exec_())
